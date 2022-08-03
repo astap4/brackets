6406743b4f2d85049b13d4e41b2673497b5ed2f3
Alina <astapchik.1993@mail.ru>
2022-08-03 19:36:29 +0300

    feat: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsPairs = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let key = bracketsConfig[i][0];
+    let value = bracketsConfig[i][1];
+    bracketsPairs[key] = value;
+  }
+  console.log(bracketsPairs)
+  let stack = [str[0]];
+  for (let i = 1; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let previousSymbol = stack[stack.length - 1]
+    if (currentSymbol != bracketsPairs[previousSymbol]) {
+      stack.push(currentSymbol);
+      console.log(stack) //добавляем в стек, если это открывающаяся скобка
+    }
+    else {
+      stack.pop();
+      console.log(stack)}
+  }
+  return stack.length === 0;
 }
